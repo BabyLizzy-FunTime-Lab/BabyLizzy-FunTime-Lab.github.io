9ff46e6a8001c82a970abfdd9b1650e3a2c371e0
Darthporkius <cholochullo@hotmail.com>
2025-08-22 12:48:06 +0200

    section--container and fotovideo--container have been updated with style and background image

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
+
 # Gitaren 
-This was my HTML5 & CSS3 final project. As you can imagine it's 99% HTML5 & CSS3. Also....I like guitars. 
+This was my HTML5 & CSS3 final project. As you can imagine it's 99% HTML5 & CSS3. Also....I like guitars. </br>
+![Logo](images/logo/horns_426x640.png)
--- js/main.js
+++ js/main.js
@@ -1,9 +1,1 @@
-var map;
-var guitarcenter = {lat: 51.9172347, lng: 4.495705};
-var marker;
-
-function initMap() {
-map = new google.maps.Map(document.getElementById('map'), {
-  zoom: 12, center: guitarcenter });
-marker = new google.maps.Marker({position: guitarcenter, map: map});
-}
+// I'm gonna add animations
